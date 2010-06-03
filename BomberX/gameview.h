abba8d2fac1b4ec7f941d52b2330607ca37685d6
n.verinaud <n.verinaud@eb40956e-4c2f-0bef-11c1-90eef6b78727>
2010-06-03 13:47:23 +0000

    Version final moche owi

--- BomberX/gameview.h
+++ BomberX/gameview.h
@@ -10,6 +10,7 @@
 #include <QGraphicsItem>
 #include <QLayout>
 #include <QKeyEvent>
+#include <QMessageBox>
 
 #include "base.h"
 
@@ -27,20 +28,31 @@ public:
     void removeCaseAtOffset(int, int, TypeDeCase);
     void displayExplosionAtOffset(int, int);
 
+    void killBomberman(int);
+
+    void endOfTheGame();
+
     QGraphicsPixmapItem* bomber1, *bomber2, *bomber3, *bomber4;
     GameController* gameController;
 
 public slots:
-    void actionPlay();
+    void removeExplosionAtOffset();
 
 protected:
     void keyPressEvent(QKeyEvent* e);
 
 private:
-    QPushButton *playButton, *quitButton;
+    QPushButton *quitButton;
     QGraphicsScene *scene;
     QGraphicsView *view;
 
+    int currentExplosionX;
+    int currentExplosionY;
+
+    QTimer* explosionRemoving;
+
+    bool bomber1isAlive;
+    bool bomber4isAlive;
 };
 
 #endif // GAMEVIEW_H
